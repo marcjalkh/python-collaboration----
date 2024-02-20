3ef95e04549fe436a0680d95a1556a7610c223bf
marcjalkh <160654703+marcjalkh@users.noreply.github.com>
2024-02-20 17:17:29 -0500

    Updated todo_list.py new feature priority_list.py

--- todo_list.py
+++ todo_list.py
@@ -30,3 +30,17 @@ while c<t:
     else:
         print("unavailable task...")
     c=c+1
+#this function sorts tasks based on user priority (no markings)
+def prio(l):
+    print("The list is oredered as such> "+str(l))
+    print("Based on the above order set priorities as numbers (1 as highest priority)>>>")
+    p=[]
+    pl={}
+    for i in range(0,len(l)-1,1):
+        print("Set priority for task "+l[i]+"> ")
+        pn=int(input())
+        p.append(pn)
+    for i in range(0,len(l)-1,1):
+        pl[l[i]]=p[i]
+    print("Numbered todo list "+str(pl))
+prio(l)
